d8f2f7bfb6412830e36dd30c49a845fee1caa0ed
dulkifal <dulkifilibacker@gmail.com>
2022-09-10 10:22:19 +0530

    next warns and axios

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a3b3451302f4481c"; }
+/******/ 		__webpack_require__.h = function() { return "9f612b620cdece4b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
