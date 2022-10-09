70da0581f505dc32041c6e202dfd5c68088c5a06
ashrafp216 <ashrafp216@gmail.com>
2022-10-10 00:59:52 +0530

    Layout header done.

---
+++ helpers/functions.js
@@ -0,0 +1,15 @@
+const helpers = {
+    // replaceSpaceWithBr: (str) => {
+    //     str = "ashraf p"
+    //     for (var i = 0; i < str.length; i++) {
+    //         let newOut;
+    //         if (i == ' ') {
+
+    //         } else {
+    //             newOut += i
+    //         }
+    //         alert(str[i]);
+    //     }
+    // },
+}
+export default helpers
