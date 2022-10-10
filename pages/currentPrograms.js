194eaf4a22e4515bc40cd526b2689f1ebc45772e
Althafpaika <althafpaika@gmail.com>
2022-10-10 10:44:42 +0530

    currentPrograms description added

--- pages/currentPrograms.js
+++ pages/currentPrograms.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "../styles/currentPrograms.module.css";
 import big_logo from "../public/assets/big_logo_.png";
+import people_icon from "../public/assets/people_icon.png"
 
 
 export default function CurrentPrograms() {
@@ -12,6 +13,13 @@ export default function CurrentPrograms() {
                 </div>
                 <h1>CURRENT<br />PROGRAMS</h1>
             </header>
+            <div className={styles.description}>
+                <div className={styles.img}>
+                    <Image src={people_icon} layout={"responsive"}></Image>
+                </div>
+                <h2>New programs are here.</h2>
+                <p>At the fest of Sibaq 22, we aim to sharpen the immature skills by several means and through a number of competitions. The various skills of writing, oratory, aptitude, versatility and virtuosity are improved to equip the students with best in class expertise and professionalism. Engagement of 32 UG colleges and more than 2600 candidates leaves nothing more to tell !.</p>
+            </div>
         </div>
     )
 }
