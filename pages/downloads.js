1dcf82c70c07524cddfe3ebf8990bc65b95a1492
Althafpaika <althafpaika@gmail.com>
2022-10-13 15:20:12 +0530

    downloads page cards svg bugs fixed, nics section added

--- pages/downloads.js
+++ pages/downloads.js
@@ -21,6 +21,80 @@ function Downloads() {
                             </div>
                             <p className={styles.fileType}>PDF</p>
                         </div>
+                        <div className={styles.card}>
+                            <div className={styles.title}>
+                                <p>By low<br/> Sibaq '22<br/> Non NIICS</p>
+                            </div>
+                            <p className={styles.date}>12/07/2022, 02:00 pm</p>
+                            <div className={styles.fileImg}>
+                                <Filesvg className={styles.svg}/>
+                            </div>
+                            <p className={styles.fileType}>PDF</p>
+                        </div>
+                        <div className={styles.card}>
+                            <div className={styles.title}>
+                                <p>By low<br/> Sibaq '22<br/> Non NIICS</p>
+                            </div>
+                            <p className={styles.date}>12/07/2022, 02:00 pm</p>
+                            <div className={styles.fileImg}>
+                                <Filesvg className={styles.svg}/>
+                            </div>
+                            <p className={styles.fileType}>PDF</p>
+                        </div>
+                        <div className={styles.card}>
+                            <div className={styles.title}>
+                                <p>By low<br/> Sibaq '22<br/> Non NIICS</p>
+                            </div>
+                            <p className={styles.date}>12/07/2022, 02:00 pm</p>
+                            <div className={styles.fileImg}>
+                                <Filesvg className={styles.svg}/>
+                            </div>
+                            <p className={styles.fileType}>PDF</p>
+                        </div>
+                    </div>
+                    <div className={styles.hrLine}></div>
+                    <h3>NIICS</h3>
+                    <div className={styles.cards}>
+                        <div className={styles.card}>
+                            <div className={styles.title}>
+                                <p>By low<br/> Sibaq '22<br/> Non NIICS</p>
+                            </div>
+                            <p className={styles.date}>12/07/2022, 02:00 pm</p>
+                            <div className={styles.fileImg}>
+                                <Filesvg className={styles.svg}/>
+                            </div>
+                            <p className={styles.fileType}>PDF</p>
+                        </div>
+                        <div className={styles.card}>
+                            <div className={styles.title}>
+                                <p>By low<br/> Sibaq '22<br/> Non NIICS</p>
+                            </div>
+                            <p className={styles.date}>12/07/2022, 02:00 pm</p>
+                            <div className={styles.fileImg}>
+                                <Filesvg className={styles.svg}/>
+                            </div>
+                            <p className={styles.fileType}>PDF</p>
+                        </div>
+                        <div className={styles.card}>
+                            <div className={styles.title}>
+                                <p>By low<br/> Sibaq '22<br/> Non NIICS</p>
+                            </div>
+                            <p className={styles.date}>12/07/2022, 02:00 pm</p>
+                            <div className={styles.fileImg}>
+                                <Filesvg className={styles.svg}/>
+                            </div>
+                            <p className={styles.fileType}>PDF</p>
+                        </div>
+                        <div className={styles.card}>
+                            <div className={styles.title}>
+                                <p>By low<br/> Sibaq '22<br/> Non NIICS</p>
+                            </div>
+                            <p className={styles.date}>12/07/2022, 02:00 pm</p>
+                            <div className={styles.fileImg}>
+                                <Filesvg className={styles.svg}/>
+                            </div>
+                            <p className={styles.fileType}>PDF</p>
+                        </div>
                     </div>
                 </div>
             </section>
