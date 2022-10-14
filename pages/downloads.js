bef81789848bab95a7da250fe1a51b6bafc53830
dulkifal <dulkifilibacker@gmail.com>
2022-10-14 09:15:45 +0530

    yarn lock change

--- pages/downloads.js
+++ pages/downloads.js
@@ -2,8 +2,11 @@ import React from 'react'
 import Layout from '../components/layout'
 import styles from "../styles/downloads.module.css"
 import Filesvg from "../public/assets/files.svg"
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 function Downloads() {
+    const router = useRouter()
     return (
         <Layout title='Downloads'>
             <section className={styles.downloads}>
@@ -11,7 +14,13 @@ function Downloads() {
                     <div className={styles.hrLine}></div>
                     <h3>General</h3>
                     <div className={styles.cards}>
-                        <div className={styles.card}>
+                        <a
+                            href="/pdf/BY-LAW.pdf"
+                            alt="alt text"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >Download FIle</a>
+                        <div className={styles.card}     >
                             <div className={styles.title}>
                                 <p>By low<br/> Sibaq '22<br/> Non NIICS</p>
                             </div>
