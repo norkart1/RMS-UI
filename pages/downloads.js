c9646a5d934456e43c0c5c8ca1a17ba4d8616f36
ashrafp216 <ashrafp216@gmail.com>
2022-10-15 07:32:41 +0530

    downloads changed to dynamic

--- pages/downloads.js
+++ pages/downloads.js
@@ -7,108 +7,79 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 
 function Downloads() {
+    const downloads = [
+        {
+            category: "general",
+            files: [
+                {
+                    name: "Download 1",
+                    link: "https://www.google.com",
+                    size: "1.5 MB",
+                    type: "PDF",
+                    date: "2020-01-01",
+                },
+                {
+                    name: "Download 2",
+                    link: "https://www.google.com",
+                    size: "1.5 MB",
+                    type: "PDF",
+                    date: "2020-01-01",
+                },
+            ]
+        },
+        {
+            category: "NIICS",
+            files: [
+                {
+                    name: "Download 1 for NIICS",
+                    link: "https://www.google.com",
+                    size: "1.5 MB",
+                    type: "PDF",
+                    date: "2020-01-01",
+                },
+                {
+                    name: "Download 2",
+                    link: "https://www.google.com",
+                    size: "1.5 MB",
+                    type: "PDF",
+                    date: "2020-01-01",
+                },
+            ]
+        }
+
+    ]
     const router = useRouter()
     return (
         <Layout title='Downloads'>
             <section className={styles.downloads}>
                 <div className={`${styles.container} container `}>
-                    <div className={styles.hrLine}></div>
-                    <h3>General</h3>
-                    <div className={styles.cards}>
+                    {
+                        downloads.map((download, index) => (
+                            <div className={styles.download_category} key={index}>
+                                <h3>{download.category}</h3>
+                                <div className={styles.hrLine}></div>
+                                <div className={styles.cards}>
+                                    {
+                                        download.files.map((file, index) => (
+                                            <div className={styles.card} key={index}>
+                                                <div className={styles.title}>
+                                                    {file.name}
+                                                </div>
+                                                <p className={styles.date}>{file.date}</p>
+                                                <div className={styles.fileImg}>
+                                                    <Filesvg className={styles.svg} />
+                                                </div>
+                                                <p className={styles.fileType}>{file.type}</p>
+                                                <Downloadsvg className={styles.dlSvg} />
+                                            </div>
+                                        ))
+                                    }
 
-                        <div className={styles.card}>
-                            <div className={styles.title}>
-                                <p>By low<br /> Sibaq '22<br /> Non NIICS</p>
+                                </div>
                             </div>
-                            {/* <p className={styles.date}>12/07/2022, 02:00 pm</p> */}
-                            <div className={styles.fileImg}>
-                                <Filesvg className={styles.svg} />
-                            </div>
-                            <p className={styles.fileType}>PDF</p>
-                            <Downloadsvg className={styles.dlSvg} />
-                        </div>
-                        <div className={styles.card}>
-                            <div className={styles.title}>
-                                <p>By low<br /> Sibaq '22<br /> Non NIICS</p>
-                            </div>
-                            {/* <p className={styles.date}>12/07/2022, 02:00 pm</p> */}
-                            <div className={styles.fileImg}>
-                                <Filesvg className={styles.svg} />
-                            </div>
-                            <p className={styles.fileType}>PDF</p>
-                            <Downloadsvg className={styles.dlSvg} />
-                        </div>
-                        <div className={styles.card}>
-                            <div className={styles.title}>
-                                <p>By low<br /> Sibaq '22<br /> Non NIICS</p>
-                            </div>
-                            {/* <p className={styles.date}>12/07/2022, 02:00 pm</p> */}
-                            <div className={styles.fileImg}>
-                                <Filesvg className={styles.svg} />
-                            </div>
-                            <p className={styles.fileType}>PDF</p>
-                            <Downloadsvg className={styles.dlSvg} />
-                        </div>
-                        <div className={styles.card}>
-                            <div className={styles.title}>
-                                <p>By low<br /> Sibaq '22<br /> Non NIICS</p>
-                            </div>
-                            {/* <p className={styles.date}>12/07/2022, 02:00 pm</p> */}
-                            <div className={styles.fileImg}>
-                                <Filesvg className={styles.svg} />
-                            </div>
-                            <p className={styles.fileType}>PDF</p>
-                            <Downloadsvg className={styles.dlSvg} />
-                        </div>
-                    </div>
-                    <div className={styles.hrLine}></div>
-                    <h3>NIICS</h3>
-                    <div className={styles.cards}>
-                        <div className={styles.card}>
-                            <div className={styles.title}>
-                                <p>By low<br /> Sibaq '22<br /> Non NIICS</p>
-                            </div>
-                            {/* <p className={styles.date}>12/07/2022, 02:00 pm</p> */}
-                            <div className={styles.fileImg}>
-                                <Filesvg className={styles.svg} />
-                            </div>
-                            <p className={styles.fileType}>PDF</p>
-                            <Downloadsvg className={styles.dlSvg} />
-                        </div>
-                        <div className={styles.card}>
-                            <div className={styles.title}>
-                                <p>By low<br /> Sibaq '22<br /> Non NIICS</p>
-                            </div>
-                            {/* <p className={styles.date}>12/07/2022, 02:00 pm</p> */}
-                            <div className={styles.fileImg}>
-                                <Filesvg className={styles.svg} />
-                            </div>
-                            <p className={styles.fileType}>PDF</p>
-                            <Downloadsvg className={styles.dlSvg} />
-                        </div>
-                        <div className={styles.card}>
-                            <div className={styles.title}>
-                                <p>By low<br /> Sibaq '22<br /> Non NIICS</p>
-                            </div>
-                            {/* <p className={styles.date}>12/07/2022, 02:00 pm</p> */}
-                            <div className={styles.fileImg}>
-                                <Filesvg className={styles.svg} />
-                            </div>
-                            <p className={styles.fileType}>PDF</p>
-                            <Downloadsvg className={styles.dlSvg} />
-                        </div>
-                        <div className={styles.card}>
-                            <div className={styles.title}>
-                                <p>By low<br /> Sibaq '22<br /> Non NIICS</p>
-                            </div>
-                            {/* <p className={styles.date}>12/07/2022, 02:00 pm</p> */}
-                            <div className={styles.fileImg}>
-                                <Filesvg className={styles.svg} />
-                            </div>
-                            <p className={styles.fileType}>PDF</p>
-                            <Downloadsvg className={styles.dlSvg} />
-                        </div>
-                    </div>
+                        ))
+                    }
+
                 </div>
             </section>
         </Layout>
