243887662c8df279f03fba4332585a4e2b6aef5e
dulkifal <dulkifilibacker@gmail.com>
2022-10-18 23:54:20 +0530

    notefication and captions

--- helpers/sample_data.js
+++ helpers/sample_data.js
@@ -15,8 +15,8 @@ More than 4000 candidates are expected to showcase their talents in a number of 
 The students from Darul Huda central campus and its 28 affiliated colleges, in and out of Kerala, and five off-campus centers will participate in the event.`,
             image: require("/public/assets/images/sibaq-logo-reveal-news.jpg").default,
             image_caption: 'Sibaq \'22 logo is revealed',
-            image_description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Phasellus egestas tellus rutrum tellus. Ac turpis egestas maecenas pharetra convallis posuere.',
-            date: '2020-01-01',
+            image_description: '  Sibaq \'22 logo is revealed',
+            date: '2022-10-11',
             tags: ['tag1', 'tag2']
         },
         {
@@ -26,8 +26,8 @@ The students from Darul Huda central campus and its 28 affiliated colleges, in a
             news_content: `Sibaq, the national arts fest, organized by Darul Huda Islamic University, is all set to commence its final edition from December 1 to 4 at the varsity campus. The selected candidates from the primary rounds will make grand finale podiums engaged and enthralled.The sibaq will finish its elimination rounds of Bidayah and Ula within November 11-13 followed by the elimination round of Thaniya and Thanawiyya within November 18 to 20, held in six affiliated academies of DHIU after the competition of Y category. Darul Huda will be in spotlight as most awaited competition items under different venues; both indoor and  outdoor, will be progressing underway . The event will be highlighted for the participation of more than 9100 students and more than 4000 candidates in 360+items from 20+ states beginning with a huge procession marked by colourful manifestations of art under concerned institutions. The hosting institution DHIU is full-fledged by its heartful prearrangement for the regional and national candidates. About three months in the making, the event focused on creating an art-oriented and Islamic-friendly fest in the values. As art can act as a mirror that reflects modern problems and a bridge between different cultural outlooks. The diversity of participants make this arts fest as the celebrations of richness and diversity of culture and creativity more than performing art.  Sibaq’22 with its vibrant  legacy upholds the stature of the charisma acquired by Darul Huda throughout it's history, creatively engaging in socio-cultural aspects. Sibaq'22 podiums will facilitate the candidates a distinctive platform to develop an constructive and competitive environment. The institution led procession will be succeeded by the flag hoisting and the  gathering. Darul Huda Islamic University has stretched its all managing directions to host the national arts fest.`,
             image: require("/public/assets/images/sibaq-gears-up.jpg").default,
             image_caption: 'Sibaq \'22 logo is revealed',
-            image_description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Phasellus egestas tellus rutrum tellus. Ac turpis egestas maecenas pharetra convallis posuere.',
-            date: '2020-01-01',
+            image_description: `SIBAQ'22 GEARS UP FOR GRAND FINALE ON December `,
+            date: '2022-10-18',
             tags: ['tag1', 'tag2']
         },
 
@@ -42,7 +42,15 @@ The students from Darul Huda central campus and its 28 affiliated colleges, in a
             description: ' Sibaq 2022 Logo was launched on October 1st, 2021. The logo was designed by the Sibaq 2022 Logo Designing Committee.',
             image: require("/public/assets/images/logo_rounded.png").default,
             link: '/news/sibaq-22-logo-is-revealed',
-            date: '2020-01-01',
+            date: '2022-10-11',
+        },
+        {
+            id: 2,
+            title: `SIBAQ'22 GEARS UP FOR GRAND FINALE ON December`,
+            description: ' Sibaq, the national arts fest, organized by Darul Huda Islamic University, is all set to commence its final edition from December 1 to 4 at the varsity campus.',
+            image: require("/public/assets/images/logo_rounded.png").default,
+            link: '/news/SIBAQ22-GEARS-UP-FOR-GRAND-FINALE-ON-December',
+            date: '2022-10-18',
         },
 
     ]
