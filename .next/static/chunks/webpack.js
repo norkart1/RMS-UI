289efaa35b36536bf0a9bc8698c9801c835c3d39
dulkifal <dulkifilibacker@gmail.com>
2022-10-18 15:50:55 +0530

    news contentss

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "083f289876772caf"; }
+/******/ 		__webpack_require__.h = function() { return "c6f4935d7cf8a985"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
