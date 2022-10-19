d36cf3b03a40c388e6bad949a899e32ad232d01e
Althafpaika <althafpaika@gmail.com>
2022-10-19 09:45:44 +0530

    gallery photos added and downloads file changed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "22d8baed32ab49ac"; }
+/******/ 		__webpack_require__.h = function() { return "aeb2666d80e63a17"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/explore.js
+++ pages/explore.js
@@ -10,30 +10,58 @@ function Gallery() {
     const images = [
         {
             id: 1,
-            image: require('../public/assets/gallery-images/1x1_1.jpg').default,
+            image: require('../public/assets/gallery-images/2x2_2.png').default,
+            likes: 5,
+            size: '2x2',
+            description: 'somthing got here in the description'
+        },
+        {
+            id: 1,
+            image: require('../public/assets/gallery-images/2x3_6.jpg').default,
+            likes: 5,
+            size: '2x3',
+            description: 'somthing got here in the description'
+        },
+        {
+            id: 1,
+            image: require('../public/assets/gallery-images/1x1_3.png').default,
             likes: 5,
             size: '1x1',
+            description: 'somthing got here in the description'
+        },
+        {
+            id: 1,
+            image: require('../public/assets/gallery-images/1x1_1.jpg').default,
+            likes: 5,
+            size: '1x2',
             description: 'somthing got here in the description'
         },
         {
             id: 1,
             image: require('../public/assets/gallery-images/1x1_2.jpg').default,
             likes: 5,
-            size: '1x1',
+            size: '2x2',
+            description: 'somthing got here in the description'
+        },
+        {
+            id: 1,
+            image: require('../public/assets/gallery-images/2x2_3.jpg').default,
+            likes: 5,
+            size: '2x2',
             description: 'somthing got here in the description'
         },
         {
             id: 1,
             image: require('../public/assets/gallery-images/2x2_1.jpg').default,
             likes: 5,
-            size: '1x1',
+            size: '2x2',
             description: 'somthing got here in the description'
         },
         {
             id: 1,
             image: require('../public/assets/gallery-images/2x3_1.jpg').default,
             likes: 5,
-            size: '2x3npm ',
+            size: '2x3',
             description: 'somthing got here in the description'
         },
         {
@@ -47,43 +75,31 @@ function Gallery() {
             id: 1,
             image: require('../public/assets/gallery-images/2x3_3.jpg').default,
             likes: 5,
-            size: '2x3',
+            size: '2x2',
             description: 'somthing got here in the description'
         },
         {
             id: 1,
             image: require('../public/assets/gallery-images/2x3_4.jpg').default,
             likes: 5,
-            size: '2x3',
+            size: '1x3',
             description: 'somthing got here in the description'
         },
         {
             id: 1,
             image: require('../public/assets/gallery-images/2x3_5.jpg').default,
             likes: 5,
-            size: '2x3',
+            size: '1x3',
             description: 'somthing got here in the description'
         },
 
 
     ]
-    // function getSpanEstimate(height, width) {
-    //     if (height > width) {
-    //         return {
-    //             gridRowEnd: `span ${Math.ceil(height / 10)}`,
-    //             gridColumnEnd: `span ${Math.ceil(width / 10)}`,
-    //         }
-    //     }
-    //     else{
-    //         return {
-    //             gridRowEnd: `span ${Math.ceil(height / 10)}`,
-    //             gridColumnEnd: `span ${Math.ceil(width / 10)}`,
-    //         }
-    //     }
-    // };
+
     function getSpanEstimate(size) {
         if (size === '1x1' || size === 'undefined') return { gridColumnEnd: 'span 1', gridRowEnd: 'span 1' }
         if (size === '1x2') return { gridColumnEnd: 'span 1', gridRowEnd: 'span 2' }
+        if (size === '1x3') return { gridColumnEnd: 'span 1', gridRowEnd: 'span 3' }
         if (size === '2x1') return { gridColumnEnd: 'span 2', gridRowEnd: 'span 1' }
         if (size === '2x2') return { gridColumnEnd: 'span 2', gridRowEnd: 'span 2' }
         if (size === '2x3') return { gridColumnEnd: 'span 2', gridRowEnd: 'span 3' }
@@ -102,12 +118,6 @@ function Gallery() {
                                 {/* <p className={styles.description}>{img.description}</p> */}
                             </article>
                         ))
-                        // images.map((img) => (
-                        //     <article className={styles.imageContainer} key={img.id}>
-                        //         <img className={styles.img} src={img.url} alt={img.alt} loading="lazy" />
-                        //         <p className={styles.description}>{img.description}</p>
-                        //     </article>
-                        // ))
                     }
                 </div>
             </section>
