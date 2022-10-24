97fa11235ef50f245e7c359a85e18ee31f5169f0
dulkifal <dulkifilibacker@gmail.com>
2022-10-25 00:08:11 +0530

    editd reviewed

--- helpers/auth.js
+++ helpers/auth.js
@@ -7,18 +7,14 @@ const refreshTokens = async () => {
   let expires_time = expire
   let expires_in = expires_time * 1000
   let expires_date = new Date(expires_in)
-  let expires_date_time = expires_date.getTime()
+  let expires_date_time = expires_date.getTime()  
   let time = expires_date_time - now_time
   // console.log(expire)
   if (time < 0) {
     console.log('token expired')
-    const token = localStorage.getItem('token');
     const refreshToken =  localStorage.getItem('refreshToken')
-    const user = JSON.parse(localStorage.getItem('user'));
     const data = {
-      token: token,
       refreshToken: refreshToken,
-      user: user,
     };
     const newToken = await baseApi({
       method: 'post',
