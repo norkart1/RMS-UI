febdcb43bd3575f9c6ebb40692e8580fc5b8169b
dulkifal <dulkifilibacker@gmail.com>
2022-10-26 12:43:50 +0530

    refresh token before 60 seconds

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,8 +1,36 @@
+import jwtdecode from "jwt-decode";
+import dayjs from "dayjs";
+import baseApi from '../api/baseApi';
+
+
+const refreshToken = async () => {
+  if(localStorage.getItem('token')){
+  const token = localStorage.getItem('token');
+  const decoded = jwtdecode(token);
+  const currentTime = dayjs().unix();
+  if (decoded.exp - currentTime < 60) {
+  try {
+    const res = await baseApi.post('/admin/refresh-token',{
+      refreshToken: localStorage.getItem('refreshToken')
+    }  )
+    console.log("refesh token");
+      localStorage.setItem('token', res.data.data.access_token);
+      localStorage.setItem('refreshToken', res.data.data.refresh_token);
+      // return res.data.data.access_token;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+}
+
+
 const logout = () => {
 
   localStorage.removeItem('token');
   localStorage.removeItem('user');
+  localStorage.removeItem('refreshToken');
   window.location.href = '/';
 }
 
-export {  logout };
+export {  logout , refreshToken};
