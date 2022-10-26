ff68caa66b5e749e40c0f077e187d011cfcb052d
dulkifal <dulkifilibacker@gmail.com>
2022-10-26 15:28:55 +0530

    news page gird

--- pages/news/index.js
+++ pages/news/index.js
@@ -12,49 +12,51 @@ export async function getStaticProps() {
             news: data.news,
 
         }
-    }}
+    }
+}
 
-    function News({ news }) {
+function News({ news }) {
 
 
-        const tagArrayToString = (tags) => {
-            let tagString = '#'
-            return tagString += tags.join(', #')
-        }
-
-        const router = useRouter()
-        return (
-            <Layout title='News'>
-                <section className={styles.news_section}>
-                        <div className={styles.news_container}>
-                            {
-                                news.map((news_item, index) => (
-                                    <div key={index} className={styles.news} onClick={() => router.push(`/news/${news_item.slug}`)}>
-                                        {news_item.image && <Image className={styles.news_img} src={news_item.image} layout='responsive' alt="sibaq at 22 darul huda art fest"></Image>}
-                                        <div className={styles.news_content}>
-                                            <h4>{news_item.heading}</h4>
-                                            <p>{news_item.news_content.slice(0, 300) + '... '}
-                                                <span className={styles.read_more}> Read more</span>
-                                            </p>
-                                            <div className={styles.news_details}>
-                                                <p className={styles.news_date}> {news_item.date}</p>
-                                                <p className={styles.news_tags}>{tagArrayToString(news_item.tags)}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                ))
-                            }
-                        </div>
-                        <div></div>
-                        <div className={styles.notification_container}>
-                            <h3>Notifications</h3>
-                            <div className={styles.line}></div>
-                            <Notifications />
-                        </div>
-                    </div>
-                </section>
-            </Layout >
-        )
+    const tagArrayToString = (tags) => {
+        let tagString = '#'
+        return tagString += tags.join(', #')
     }
 
-    export default News
+    const router = useRouter()
+    return (
+        <Layout title='News'>
+            <section className={styles.news_section}>
+                <div className={`${styles.container} container`}>
+                    <div className={styles.news_container}>
+                        {
+                            news.map((news_item, index) => (
+                                <div key={index} className={styles.news} onClick={() => router.push(`/news/${news_item.slug}`)}>
+                                    {news_item.image && <Image className={styles.news_img} src={news_item.image} layout='responsive' alt="sibaq at 22 darul huda art fest"></Image>}
+                                    <div className={styles.news_content}>
+                                        <h4>{news_item.heading}</h4>
+                                        <p>{news_item.news_content.slice(0, 300) + '... '}
+                                            <span className={styles.read_more}> Read more</span>
+                                        </p>
+                                        <div className={styles.news_details}>
+                                            <p className={styles.news_date}> {news_item.date}</p>
+                                            <p className={styles.news_tags}>{tagArrayToString(news_item.tags)}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                    <div></div>
+                    <div className={styles.notification_container}>
+                        <h3>Notifications</h3>
+                        <div className={styles.line}></div>
+                        <Notifications />
+                    </div>
+                </div>
+            </section>
+        </Layout >
+    )
+}
+
+export default News
