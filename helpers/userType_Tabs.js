121d26b823b99c13afefcdd6044a21733d005b97
dulkifal <dulkifilibacker@gmail.com>
2022-11-01 20:30:46 +0530

    controller set

--- helpers/userType_Tabs.js
+++ helpers/userType_Tabs.js
@@ -82,7 +82,7 @@ export default [
         name: 'Controllers',
         icon: '/assets/png/controllers.png',
         link: '/admin/controller',
-        isVisible: false,
+        isVisible: true,
       },
       {
         id: 9,
