bf26078475751c9feb145f53601e7a7ac59a7ab7
dulkifal <dulkifilibacker@gmail.com>
2022-11-09 10:50:45 +0530

    nics close

--- pages/portal/programs/program_registration.js
+++ pages/portal/programs/program_registration.js
@@ -138,7 +138,7 @@ console.log(prefix)
     <Portal_Layout activeTabName='programs' activeChildTabName='Register programs' userType='institute'>
       <div className={styles.pageContainer}>
         <h1>Program registration</h1>
-        {coordinator?.institute_id?.id == 41 || coordinator?.institute_id?.id == 42 || coordinator?.institute_id.session.id == 2 ? <div>
+        {coordinator?.institute_id?.id == 41 || coordinator?.institute_id?.id == 42   ? <div>
           <span data-theme='hr'></span>
           <div className={styles.dataContainer}>
 
