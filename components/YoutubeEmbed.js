b798d6ebfb1f16dd2e7919ffbc5731696c36da83
Althafpaika <althafpaika@gmail.com>
2022-11-11 09:47:18 +0530

    youtueb related vids changed

--- components/YoutubeEmbed.js
+++ components/YoutubeEmbed.js
@@ -9,7 +9,7 @@ const YoutubeEmbed = ({ embedId }) => (
             // height="480"
             width="100%"
             height="100%"
-            src={`https://www.youtube.com/embed/${embedId}`}
+            src={`https://www.youtube.com/embed/${embedId}?rel=0`}
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
