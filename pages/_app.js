91754130d7f6d555d24a39fb3e22ab9e4de0a7b3
dulkifal <dulkifilibacker@gmail.com>
2022-11-12 22:32:37 +0530

    google analythics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import '../styles/globals.css'
 
+import ReactGA from 'react-ga';
+const TRACKING_ID = "G-F5MVYGVWTX"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
