6c786cf4ffc29b59dd8f529f8e7c686315de14d8
ashrafp216 <ashrafp216@gmail.com>
2022-11-17 10:14:51 +0530

    hided remove in registered programs

--- pages/portal/programs/registered_programs.js
+++ pages/portal/programs/registered_programs.js
@@ -108,7 +108,7 @@ function Categories() {
   const loadTableData = async () => {
 
   }
-  const heads = prefix ? ['SI.', 'Program code', 'Program ', "Chest No.", "Candidate name", ''] : ['SI.', 'Program code', 'Program ', "Chest No.", "Candidate name"]
+  const heads =  ['SI.', 'Program code', 'Program ', "Chest No.", "Candidate name"]
   const candOptions = candidates?.filter(cand => cand.categoryID == catID).map((item, index) => {
     return { value: item.id, label: item.chestNO + ' - ' + item.name, chestNO: item.chestNO, name: item.name }
   })
@@ -180,7 +180,7 @@ function Categories() {
                         <td style={{ width: '19rem' }}>{prefix}{program.chestNO}</td>
                         <td style={{ width: '19rem' }}>{program.candidate.name}</td>
                         {/* <td style={{ width: '19rem' }}>{program.groupCount}</td> */}
-                        {prefix && <td style={{ width: '1rem' }}>
+                        {false && <td style={{ width: '1rem' }}>
 
                           <button height={20} fill='red' data-theme='delete' onClick={(e) => handleDelete(e, program.id)}>
                             Remove
