8d96c96d5f6cff39445d5970d44a3feb818d5e42
dulkifal <dulkifilibacker@gmail.com>
2022-11-28 12:45:12 +0530

    login redirct

--- pages/login.js
+++ pages/login.js
@@ -6,48 +6,47 @@ import baseApi from "../api/baseApi";
 export default function Redirect () {
     const router = useRouter();
 
-    // useEffect(() => {
-    //     // check if user has token
-    //     if (localStorage.getItem("token")) {
-    //         // check if user is admin
-    //         baseApi.get("/admin/user").then((res) => {
-    //             if(res.data.success) {
-    //                 router.push("/admin/dashboard");
-    //             } 
-    //         }).catch(
-    //             (err) => {
+    useEffect(() => {
+        // check if user has token
+        if (localStorage.getItem("token")) {
+            // check if user is admin
+            baseApi.get("/admin/user").then((res) => {
+                if(res.data.success) {
+                    router.push("/admin/dashboard");
+                } 
+            }).catch(
+                (err) => {
                  
-    //                 baseApi.get("/user/me").then((res) => {
-    //                     if(res.data.success) {
-    //                         router.push("/control");
-    //                     }
-    //                 }).catch((err) => {
+                    baseApi.get("/user/me").then((res) => {
+                        if(res.data.success) {
+                            router.push("/control");
+                        }
+                    }).catch((err) => {
                         
 
                         
-    //                         baseApi.get("/coordinator/me").then((res) => {
-    //                             if(res.data.success) {
-    //                                 router.push("/portal/candidates");
-    //                             }
-    //                         }).catch((err) => {
+                            baseApi.get("/coordinator/me").then((res) => {
+                                if(res.data.success) {
+                                    router.push("/portal/candidates");
+                                }
+                            }).catch((err) => {
                               
                                
-    //                                 router.push("/auth/login");
-    //                             }
-    //                         )
-    //                         });
-    //                     }
-    //                 )
+                                    router.push("/auth/login");
+                                }
+                            )
+                            });
+                        }
+                    )
                    
                   
            
-    //     } else {
-    //         router.push("/auth/login");
-    //     }
-    // }, []);
-router.push("/auth/login");
+        } else {
+            router.push("/auth/login");
+        }
+    }, []);
 
-
+    
 
 
 
