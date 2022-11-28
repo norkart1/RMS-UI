5ddcf9f0a14f1e4e7aa031f3284e6b1b6f8c63bd
MJ <jabirjbr07@gmail.com>
2022-11-28 19:13:24 +0530

    mj media

--- pages/explore.js
+++ pages/explore.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
+import ShareIcon from '@mui/icons-material/Share';
 import ImageGrid from '../components/galleryF/ImageGrid'
 import Modal from '../components/galleryF/Modal'
 import Layout from '../components/layout'
@@ -14,21 +15,34 @@ import { BaseApi } from '../helpers/functions';
 
 function Gallery() {
 
-    const router = useRouter();
+    // const router = useRouter();
     const [testImage,setTestImage] = useState([])
-    const [selectedImg, setSelectedImg] = useState(null);
-    // const [like, setLike] = useState (testImage.likes)
+    // const [selectedImg, setSelectedImg] = useState(null);
+    const [like, setLike] = useState (100)
     const [isLiked, setIsLiked] =useState(false)
-
-    // useEffect(()=>{
-    //     setIsLiked(post.likes.includes(currentUser.id))
-    // },[post.likes,currentUser.id])
-const liked = false
+    const likedId =null
+    const likeHandler =(id)=>{
+        if (!isLiked) {
+            BaseApi.post('public/media/gallery/like/'+id)
+            .then (likedId = id)           
+            setIsLiked(!isLiked)
+            setLike( like+1)
+        } else {
+            BaseApi.post('public/media/gallery/unlike/'+id)
+            setIsLiked(!isLiked)
+            setLike(like-1)
+        }
+        BaseApi.get('public/media/gallery/'+id).then((res)=>{
+            // setLike(like)
+            // console.log(res.data.data.likes);
+        })
+        // setIsLiked(!isLiked)
+        setLike(isLiked?like-1:like+1)
+    }
     const likeImg =(id)=>{
         if (!isLiked) {
             try{
                 BaseApi.post("public/media/gallery/like/"+id);
-
             }catch(err){
                 console.log(err);
             }
@@ -51,7 +65,6 @@ const liked = false
     useEffect(() => {
     BaseApi.get("public/media/gallery").then((res)=>{
         setTestImage(res.data.data)
-        console.log(res.data.data);
     })
     }, [])
     
@@ -63,16 +76,18 @@ const liked = false
         <div className={styles.gallery}>
             {testImage.map((image) => (
                 <div className={styles.images}>
-                <div className={styles.top}><LocationOnIcon className={styles.locationIcon}/><div className={styles.location}>{image.location}</div></div>
+                <div className={styles.top}><LocationOnIcon className={styles.locationIcon} /><div className={styles.location}>{image.location}</div></div>
                 <img className= {styles.photo}src={image.file.url}/>
                 <div className={styles.bottom}>
                     <div className={styles.bottom1}>
                         <div className={styles.item}>
-                   {liked ? <ThumbUpIcon/>:<ThumbUpOutlinedIcon/> }
-                   15 likes
+                   <ThumbUpIcon className={styles.likes}
+                   style={{color: isLiked ? "blue" : "white" }}
+                   onClick={()=>{likeHandler(image.id)}}/> 
+                   {like+image.likes} likes
                         </div>
                         <div className={styles.item}>
-                    <ThumbUpOutlinedIcon/>
+                    <ShareIcon/>
                     Share
                         </div>
                     
