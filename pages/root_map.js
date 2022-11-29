350336ae77f65277925762792ee81ac165f0e607
ashrafp216 <ashrafp216@gmail.com>
2022-11-29 23:00:49 +0530

    added route map

--- pages/root_map.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function root_map() {
-  return (
-    <img src="/assets/images/root_map.jpg" alt="sibaq 2022 root map" style={{width:'100%'}} />
-  )
-}
-
-export default root_map
---
+++ pages/route_map.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function root_map() {
+  return (
+    <img src="/assets/images/route_map.jpg" alt="sibaq 2022 root map" style={{width:'100%'}} />
+  )
+}
+
+export default root_map
