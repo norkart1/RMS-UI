0b7cfa496d0ebbf56e222d95da4d88afa7750738
ashrafp216 <ashrafp216@gmail.com>
2022-12-01 21:49:41 +0530

    made link to final result in dashboard

--- pages/public_portal/final_results.js
+++ pages/public_portal/final_results.js
@@ -27,7 +27,7 @@ export default function FinalResults() {
   useEffect(() => {
     const prCodeFromUrl = window.location.href.includes('#') ? window.location.href.substring(window.location.href.lastIndexOf('#') + 1) : null;
     window.prCodeFromUrl = prCodeFromUrl
-     
+
     if (prCodeFromUrl != undefined) {
       showResult({ programCode: prCodeFromUrl.toUpperCase() })
     }
@@ -37,7 +37,7 @@ export default function FinalResults() {
       .get(`public/final-result/programs/published?sessionID=${sessionId}`)
       .then((res) => {
         setPublishedPrograms(res.data.data);
-         
+
         setSearchOptions([]);
         res.data.data.map((program) => {
           setSearchOptions((prev) => [
@@ -109,7 +109,7 @@ export default function FinalResults() {
 
 
   const handleShareClick = () => {
-    
+
     const url = window.location.href
     //  
     if (url.includes('#')) {
@@ -120,7 +120,7 @@ export default function FinalResults() {
     }
 
     const urlToShare = url.substring(0, url.lastIndexOf('#')) + '#' + selectedProgram.programCode
-    navigator?.clipboard?.writeText(urlToShare).then(()=>alert('copied'))
+    navigator?.clipboard?.writeText(urlToShare).then(() => alert('copied'))
     //  
     // alert('Link copied to clipboard '+ urlToShare)
     //  
@@ -177,7 +177,8 @@ export default function FinalResults() {
                   </p>
                   <div className="flex-grow"></div>
                   <p>{timeToAgo(item.updated_at).toString()}</p>
-                  {/* <p>{new Date(item.updated_at).toString()}</p> */}
+                  {/* <p>{item.updated_at}</p> */}
+                  <p>{new Date(item.updated_at).toString()}</p>
                   {/* <p>Just now</p> */}
                 </div>
               );
@@ -194,7 +195,7 @@ export default function FinalResults() {
             <img className={s.btnClose} src="/assets/svg/close.svg" />
           </div>
           <button onClick={handleShareClick} className='btn_share'
-          style={{marginLeft:'2rem'}}
+            style={{ marginLeft: '2rem' }}
           >
             <ShareIcon />
           </button>
@@ -226,7 +227,7 @@ export default function FinalResults() {
                         </div>
                       </div>
                   }
-                  
+
                 </div>
                 <div
                   className={s.instiPhoto}
