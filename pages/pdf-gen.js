03527dedc9b50e7bead8965fa9e37584df736df9
dulkifal <dulkifilibacker@gmail.com>
2022-12-03 14:55:16 +0530

    pdf gen

--- pages/pdf-gen.js
+++ pages/pdf-gen.js
@@ -1,55 +1,55 @@
 import { useRouter } from "next/router";
-import React, { useEffect, useState } from 'react';
-import styles from "../styles/pdf.module.css"
+import React, { useEffect, useState } from "react";
+import styles from "../styles/pdf.module.css";
 
 import baseApi from "../api/baseApi";
 
 export default function Pdfgen() {
-    const [programData, setProgramData] = useState();
-    useEffect(() => {
-        result()
-    }, [])
-  const result =async()=>{
+  const [programData, setProgramData] = useState();
+  useEffect(() => {
+    result();
+  }, []);
+  const result = async () => {
+    await baseApi
+      .get(
+        `user/final-result/program/pp/${localStorage.getItem("toPrintCode")}`
+      )
+      .then((res) => {
+        setProgramData(res.data.data);
+      });
+  };
 
-     await baseApi
-       .get(`user/final-result/program/${localStorage.getItem("toPrintCode")}`)
-       .then((res) => {
-         setProgramData(res.data.data);
-       });
-    } 
-    console.log(programData && programData[0]?.programName);
-    const savePDF = async() => {
-        baseApi.get('/pdf', {
-            responseType: 'arraybuffer',
-            // baseURL: 'http://localhost:3003',
-            headers: {
-                'Accept': 'application/pdf'
-            }
+  const savePDF = async () => {
+    baseApi
+      .get("/pdf", {
+        responseType: "arraybuffer",
 
-        }).then((res) => {
-          
-            const blob = new Blob([res.data], { type: 'application/pdf' });
-            const link = document.createElement('a');
-            link.href = window.URL.createObjectURL(blob);
-            link.download = 'program.pdf';
-            link.click();
-        })
-    }
+        headers: {
+          Accept: "application/pdf",
+        },
+      })
+      .then((res) => {
+        const blob = new Blob([res.data], { type: "application/pdf" });
+        const link = document.createElement("a");
+        link.href = window.URL.createObjectURL(blob);
+        link.download = "program.pdf";
+        link.click();
+      });
+  };
 
   return (
-<>
-<h1 className={styles.heading}>{ programData && programData[0]?.programName}</h1>
-<h2 className={styles.heading2}>{ programData && programData[0]?.programCode}</h2>
-<table className={styles.styledtable}>
-    <thead>
-    {/* <tr>Program</tr>
-    <tr>{programData && programData[0].programName}</tr>
-    <tr>Code</tr>
-    <tr>{programData && programData[0].code}</tr> */}
-        <tr>
+    <>
+      <h1 className={styles.heading}>
+        {programData && programData[0]?.programName}
+      </h1>
+      <h2 className={styles.heading2}>
+        {programData && programData[0]?.programCode}
+      </h2>
+      <table className={styles.styledtable}>
+        <thead>
+          <tr>
             <th>SL.no</th>
-            {/* <th>Program Code</th> */}
-            {/* <th>Program Name</th> */}
+
             <th>Chest No</th>
             <th>Candidate Name</th>
             <th>Institute short Name</th>
@@ -57,43 +57,28 @@ export default function Pdfgen() {
             <th>Position</th>
             <th>Grade</th>
             <th>Points</th>
-        </tr>
-    </thead>
-    <tbody>
-        {/* <tr>
-            <td>1</td>
-            <td>BW11</td>
-            <td>Poem Writng</td>
-            <td>2001</td>
-            <td>Student 1</td>
-            <td>IUAC-TANUR</td>
-            <td>Ishlahul Uloom</td>
-            <td>1st</td>
-            <td>A</td>
-            <td>8</td>
-            </tr>
-        */}
-            {programData&& programData?.map((item,index) => {
-                return (
-                    <tr>
-                        <td>{index+1}</td>
-                        {/* <td>{item.programCode}</td> */}
-                        {/* <td>{item.programName}</td> */}
-                        <td>{item.chestNO}</td>
-                        <td>{item.candidate.name}</td>
-                        <td>{item.institute.shortName}</td>
-                        <td>{item.institute.name}</td>
-                        <td>{item.position}</td>
-                        <td>{item.grade}</td>
-                        <td>{item.point}</td>
-                    </tr>
-                );
+          </tr>
+        </thead>
+        <tbody>
+          {programData &&
+            programData?.map((item, index) => {
+              return (
+                <tr>
+                  <td>{index + 1}</td>
+
+                  <td>{item.chestNO}</td>
+                  <td>{item.candidate.name}</td>
+                  <td>{item.institute.shortName}</td>
+                  <td>{item.institute.name}</td>
+                  <td>{item.position}</td>
+                  <td>{item.grade}</td>
+                  <td>{item.point}</td>
+                </tr>
+              );
             })}
-        
-        {/* <!-- and so on... --> */}
-    </tbody>
-</table>
-<button onClick={savePDF}>print</button>
-            </>
+        </tbody>
+      </table>
+      <button onClick={savePDF}>print</button>
+    </>
   );
 }
