0545c0ef5a9546bd69b4b6c5fc30f63b09f1cc55
muhammed shakir va <vashakir245@gmail.com>
2022-12-03 12:51:27 +0530

    .

--- pages/tvshow_2.js
+++ pages/tvshow_2.js
@@ -1,10 +1,24 @@
+import { useRouter } from 'next/router'
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 import TvShowTable from '../components/TvShowTable'
 
 export default function Tvshow_1() {
+  const router = useRouter()
+  const [maxCount, setMaxCount] = useState(50)
+  useEffect(() => {
+    setTimeout(() => {
+
+      router.push('/tvshow_1')
+
+    }, 1000 * 60 * 2);
+
+
+  }, [])
   return (
     <div>
-        <TvShowTable sessionID={2} maxCount={10}  />
+      <TvShowTable sessionID={2} maxCount={maxCount} />
     </div>
   )
 }
